71d7f346349646672a301941e1e248c28c775edd
alisac <andy.lisac@consultig-for.accor.com>
2018-06-08 08:59:06 +0200

    finished createNewBlock function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -4,5 +4,16 @@ function Blockchain() {
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash
+  };
+
+  this.pendingTransactions = [];
+  this.chain.push(newBlock);
 
 }
