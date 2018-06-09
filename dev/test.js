e54f386887bc3c48bdde132444ae6e46cb110a7c
arizona2014 <andy.lisac@gmail.com>
2018-06-09 21:29:13 +0200

    testing several transactions

--- dev/test.js
+++ dev/test.js
@@ -5,4 +5,8 @@ bitcoin.createNewBlock(555,'DSFD5445TLKLJ','JKNE9EJIE?E');
 bitcoin.createNewTransaction(100, 'ALEXIHDD9DJH9', 'JENXN9JSD9D0D');
 bitcoin.createNewBlock(11,'DZDZDZDZDZ','XSEEDEDED?E');
 
+bitcoin.createNewTransaction(55, 'ALEXIHDD9DJH9', 'JENXN9JSD9D0D');
+bitcoin.createNewTransaction(1600, 'ALEXIHDD9DJH9', 'JENXN9JSD9D0D');
+bitcoin.createNewTransaction(560, 'ALEXIHDD9DJH9', 'JENXN9JSD9D0D');
+
 console.log(bitcoin);
