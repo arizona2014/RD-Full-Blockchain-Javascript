d09aa42fcdacd911ac1c5b5c8bec0346249dac51
arizona2014 <andy.lisac@gmail.com>
2018-06-09 21:23:45 +0200

    testing

--- dev/test.js
+++ dev/test.js
@@ -1,3 +1,8 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
+bitcoin.createNewBlock(555,'DSFD5445TLKLJ','JKNE9EJIE?E');
+bitcoin.createNewTransaction(100, 'ALEXIHDD9DJH9', 'JENXN9JSD9D0D');
+bitcoin.createNewBlock(11,'DZDZDZDZDZ','XSEEDEDED?E');
+
+console.log(bitcoin);
