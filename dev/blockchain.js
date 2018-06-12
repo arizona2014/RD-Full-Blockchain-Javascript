637940f55a8865f96b93a68908a346210dc27f69
arizona2014 <andy.lisac@gmail.com>
2018-06-12 16:47:23 +0200

    loggin all hashes

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -52,6 +52,7 @@ Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData
   while( hash.substr(0,4) !== '0000' ) {
     nonce++;
     hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    console.log(hash);
   }
   return nonce;
 }
