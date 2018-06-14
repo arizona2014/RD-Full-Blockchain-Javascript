b29de6af2c8e34e2f424f36da5016be812c663ad
arizona2014 <andy.lisac@gmail.com>
2018-06-14 08:41:44 +0200

    simple api in expressjs

--- dev/api.js
+++ dev/api.js
@@ -1,3 +1,11 @@
-/**
- * Created by Arizona on 6/13/2018.
- */
+var express = require('express');
+var app = express();
+require('dotenv').config();
+
+
+app.get('/', function (req, res) {
+  res.json({message: 'Hello world'});
+});
+
+console.log('Running API on port ', process.env.APP_PORT);
+app.listen(process.env.APP_PORT);
