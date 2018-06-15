5ca48968909def9e6f7e45e97c36510aa8864f57
arizona2014 <andy.lisac@gmail.com>
2018-06-15 16:42:35 +0200

    presenting blockchain api functions

--- dev/api.js
+++ dev/api.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
 
+const bitcoin = new Blockchain();
 
-app.get('/', function (req, res) {
-  res.json({message: 'Hello world'});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
 });
 
-console.log('Running API on port ', process.env.APP_PORT);
-app.listen(process.env.APP_PORT);
+app.post('/transaction', function (req, res) {
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}.`});
+});
+
+app.get('/mine', function (req, res) {
+  const newBlock = bitcoin.createNewBlock();
+});
+
+app.listen(process.env.APP_PORT, function() {
+  console.log('Running API on port -> ', process.env.APP_PORT);
+});
